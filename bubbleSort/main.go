06f42a82f89ecdef8d38e4b87d91e65af398686d
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-07-28 20:10:02 +0200

    finished swap func

--- bubbleSort/main.go
+++ bubbleSort/main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	var numbers []int = []int{5, 4, 2, 3, 1, 0}
-	fmt.Println("Our list of numbers is:", numbers)
+
+	fmt.Println("Before swapping:", numbers)
+
 	swap(numbers)
+	fmt.Println("After 1 swap:", numbers)
+
 }
 
 func swap(numbers []int) {
@@ -20,7 +24,11 @@ func swap(numbers []int) {
 		var firstNumber int = numbers[firstIndex]
 		var secondNumber int = numbers[secondIndex]
 
-		fmt.Println("Comparing : ", firstNumber, secondNumber)
+		if firstNumber > secondNumber {
+			// swap the numbers
+			numbers[firstIndex] = secondNumber
+			numbers[secondIndex] = firstNumber
+		}
 
 		// Increase indexes
 		firstIndex++
