00a0283710a469c4c83df58cdad605a2649f1c44
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-07-28 20:00:14 +0200

    init swap func

--- bubbleSort/main.go
+++ bubbleSort/main.go
@@ -4,4 +4,18 @@ func main() {
 
 }
 
+func swap(numbers []int) {
 
+	var N int = len(numbers)
+	var firstIndex int = 0
+	var secondIndex int = 1
+
+	// while secondIndex is smaller than numbers len
+	for secondIndex < N {
+		// do somethig
+
+		// Increase indexes
+		firstIndex++
+		secondIndex++
+	}
+}
